35eff386b9c1b8cd2a2d1bb843ed8afd61d7d563
cotasi <cotasi135@gmail.com>
2023-12-13 16:21:25 +0900

    sj

---
+++ src/sj_component/TopBanner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TopBanner = () => {
+    return (
+        <div>
+            <h2>새로운 회원가입 이벤트</h2>
+            <div>
+                <a href="#">이벤트 확인하러 가기</a>
+
+            </div>
+        </div>
+    );
+};
+
+export default TopBanner;
