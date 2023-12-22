3ce6da3aa9e680576bd499b07d07b4f811a63e57
User -2023EMGRM\Administrator <zxc770629@gmail.com>
2023-12-22 16:01:46 +0900

    (html->React) QuickMenu

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import End from './pjh/End'
+import QuickManu from './pjh/QuickMenu'
 
 function Footer(props) {
     return (
         <div>
-           
-        <End data = {props}></End>
+
+            <End data={props}></End>
+
+            <QuickManu></QuickManu>
 
         </div>
     );
